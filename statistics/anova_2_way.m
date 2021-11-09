1db42f54e8a807072e201a464f403932e0658e1a
Ciaran Cooney <35545610+cfcooney@users.noreply.github.com>
2021-11-09 20:25:06 +0000

    Update anova_2_way.m
    Added lines for several statisitcal analyses.

--- statistics/anova_2_way.m
+++ statistics/anova_2_way.m
@@ -11,11 +11,13 @@
 % Condition 2, Replication 2       7      |       9
 % Condition 3, Replication 1      18      |      16
 % Condition 3, Replication 2      11      |      10
-
+%%
 clear all
 path = '/';
 
-y = xlsread([path 'testing_data.xlsx'],'overt', 'B2:D57');
+%y = xlsread([path 'test_data.xlsx'],'2-way', 'B2:C70');
+%path = 'C:/Users/cfcoo/OneDrive - Dundalk Institute of Technology/Study_3/Multimodal/';
+y = xlsread([path 'testing_data.xlsx'],'overt', 'B2:D43');
 
 replications = 28; % number of sessions
 
@@ -28,12 +30,90 @@ tbl
 figure
 % Pairwise comparison of the row data
 [cR, mR, hR, nmsR] = multcompare(stats,'alpha',.05,'Estimate','row','ctype','hsd'); %p-values returned in 'c' variable.
-  
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
 clear all
 path = '/';
 
-y = xlsread([path 'testing_data.xlsx'],'imagined', 'B2:D43');
+y = xlsread([path 'results_4_stats.xlsx'],'imagined', 'B2:D43');
+
+replications = 21; % number of sessions
+
+[p,tbl,stats] = anova2(y, replications);
+tbl
+
+% Pairwise comparison of the column data
+[c, m, h, nms] = multcompare(stats,'alpha',.05,'ctype','hsd'); %p-values returned in 'c' variable.
+
+figure
+% Pairwise comparison of the row data
+[cR, mR, hR, nmsR] = multcompare(stats,'alpha',.05,'Estimate','row','ctype','hsd'); %p-values returned in 'c' variable.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+clear all
+path = '/';
+
+y = xlsread([path 'results_4_stats.xlsx'],'bim_eeg_ov', 'B2:c169');
+
+replications = 28; % number of sessions
+
+[p,tbl,stats] = anova2(y, replications);
+tbl
+
+% Pairwise comparison of the column data
+[c, m, h, nms] = multcompare(stats,'alpha',.05,'ctype','hsd'); %p-values returned in 'c' variable.
+
+figure
+% Pairwise comparison of the row data
+[cR, mR, hR, nmsR] = multcompare(stats,'alpha',.05,'Estimate','row','ctype','hsd'); %p-values returned in 'c' variable.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+clear all
+path = '/';
+
+y = xlsread([path 'results_4_stats.xlsx'],'bim_eeg_im', 'B2:c127');
+
+replications = 21; % number of sessions
+
+[p,tbl,stats] = anova2(y, replications);
+tbl
+
+% Pairwise comparison of the column data
+[c, m, h, nms] = multcompare(stats,'alpha',.05,'ctype','hsd'); %p-values returned in 'c' variable.
+
+figure
+% Pairwise comparison of the row data
+[cR, mR, hR, nmsR] = multcompare(stats,'alpha',.05,'Estimate','row','ctype','hsd'); %p-values returned in 'c' variable.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%%
+clear all
+path = '/';
+
+y = xlsread([path 'results_4_stats.xlsx'],'bim_fnirs_ov', 'B2:c169');
+
+replications = 28; % number of sessions
+
+[p,tbl,stats] = anova2(y, replications);
+tbl
+
+% Pairwise comparison of the column data
+[c, m, h, nms] = multcompare(stats,'alpha',.05,'ctype','hsd'); %p-values returned in 'c' variable.
+
+figure
+% Pairwise comparison of the row data
+[cR, mR, hR, nmsR] = multcompare(stats,'alpha',.05,'Estimate','row','ctype','hsd'); %p-values returned in 'c' variable.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+clear all
+path = '/';
+
+y = xlsread([path 'results_4_stats.xlsx'],'bim_fnirs_im', 'B2:c127');
 
 replications = 21; % number of sessions
 
